2653d855ff710d80b8777e2c5fd0fa8250869ad7
OdiousImp2604 <74424076+OdiousImp2604@users.noreply.github.com>
2021-01-19 18:18:35 +0000

    Chomd -x Weather Script!

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ pushd SwayEOS >/dev/null
 cp -R .config /home/$NEW_USER/
 cp -R .config ~/                                                    
 chmod -R +x ~/.config/Sway/scripts /home/$NEW_USER/.config/Sway/scripts
+chmod +x ~/home/$NEW_USER/.config/waybar/scripts/weather.sh
 cp .profile ~/
 cp .profile /home/$NEW_USER/
 chown -R $NEW_USER:users /home/$NEW_USER/.config /home/$NEW_USER/.profile
