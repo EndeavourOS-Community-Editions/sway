a9ac801220c173b95efc4e969bf78cbf2a5e0b54
Morten Bendtsen <mail@mbendtsen.dk>
2021-01-19 15:33:49 +0100

    setup.sh added

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#bin/sh
+git clone https://github.com/OdiousImp2604/SwayEOS.git
+pushd SwayEOS >/dev/null
+cp -R .config /home/$NEW_USER/
+cp -R .config ~/                                                    
+chmod -R +x ~/.config/Sway/scripts /home/$NEW_USER/.config/Sway/scripts
+cp .profile ~/
+cp .profile /home/$NEW_USER/
+chown -R $NEW_USER:users /home/$NEW_USER/.config /home/$NEW_USER/.profile
+popd >/dev/null
+rm -rf SwayEOS
