00b96f38ef10b123055c18aa5af652d19150a736
morten-b <32033374+morten-b@users.noreply.github.com>
2021-01-21 11:29:08 +0100

    Create power-menu.sh

---
+++ .config/waybar/scripts/power-menu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+entries="Logout Suspend Reboot Shutdown"
+
+selected=$(printf '%s\n' $entries | wofi --width 300 --height 150 --show=dmenu --cache-file /dev/null | awk '{print tolower($1)}')
+
+case $selected in
+  logout)
+    swaymsg exit;;
+  suspend)
+    exec systemctl suspend;;
+  reboot)
+    exec systemctl reboot;;
+  shutdown)
+    exec systemctl poweroff -i;;
+esac
