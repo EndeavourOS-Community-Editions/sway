c277be95679f348f2d03e4d6f9967f160c9f3a04
morten-b <32033374+morten-b@users.noreply.github.com>
2021-01-22 09:10:26 +0100

    Update testinstall.sh

--- testinstall.sh
+++ testinstall.sh
@@ -7,6 +7,6 @@ cp .profile ~/.profile
 cp .gtkrc-2.0 ~/.gtkrc-2.0
 chmod -R +x ~/.config/sway/scripts
 chmod -R +x ~/.config/waybar/scripts
-sudo pacman -S --needed --noconfirm - < packages-repository.txt
+sudo pacman -Syu --needed --noconfirm - < packages-repository.txt
 systemctl enable lightdm
 systemctl start lightdm
