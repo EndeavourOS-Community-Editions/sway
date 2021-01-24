ea27a4034e0b269827c46924a115eacb44cb509a
morten-b <32033374+morten-b@users.noreply.github.com>
2021-01-24 22:18:32 +0100

    Update screenshot.sh

--- .config/sway/scripts/screenshot.sh
+++ .config/sway/scripts/screenshot.sh
@@ -2,7 +2,7 @@
  
 entries="Active Screen Output Area Window"
  
-selected=$(printf '%s\n' $entries | wofi --style=$HOME/.config/wofi/style.power.css --conf=$HOME/.config/wofi/config.screenshot | awk '{print tolower($1)}')
+selected=$(printf '%s\n' $entries | wofi --style=$HOME/.config/wofi/style.widgets.css --conf=$HOME/.config/wofi/config.screenshot | awk '{print tolower($1)}')
  
 case $selected in
   active)
