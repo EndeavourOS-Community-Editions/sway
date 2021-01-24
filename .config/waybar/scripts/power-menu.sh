84a150d5b7f8b01315f611628c898d6d95b33fd4
morten-b <32033374+morten-b@users.noreply.github.com>
2021-01-24 22:16:05 +0100

    Add files via upload

--- .config/waybar/scripts/power-menu.sh
+++ .config/waybar/scripts/power-menu.sh
@@ -2,7 +2,7 @@
 
 entries="Logout Suspend Reboot Shutdown"
 
-selected=$(printf '%s\n' $entries | wofi --width 100 --height 20 --show=dmenu --cache-file /dev/null | awk '{print tolower($1)}')
+selected=$(printf '%s\n' $entries | wofi --conf=$HOME/.config/wofi/config.power --style=$HOME/.config/wofi/style.power.css | awk '{print tolower($1)}')
 
 case $selected in
   logout)
