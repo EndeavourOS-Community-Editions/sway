4b7ba266f996ee369945c6a1c9baea97479bce5c
morten-b <32033374+morten-b@users.noreply.github.com>
2021-01-24 14:25:33 +0100

    Add greeter conf

--- setup.sh
+++ setup.sh
@@ -2,11 +2,12 @@
 git clone https://github.com/OdiousImp2604/SwayEOS.git
 pushd SwayEOS >/dev/null
 cp -R .config /home/$NEW_USER/
-cp -R .config ~/                                                    
-chmod -R +x ~/.config/Sway/scripts /home/$NEW_USER/.config/Sway/scripts
-chmod +x ~/home/$NEW_USER/.config/waybar/scripts/weather.sh
+cp -R .config ~/
 cp .profile ~/
 cp .profile /home/$NEW_USER/
+cp lightdm-gtk-greeter.conf /etc/lightdm/
+chmod -R +x ~/.config/Sway/scripts /home/$NEW_USER/.config/Sway/scripts
+chmod +x ~/home/$NEW_USER/.config/waybar/scripts/weather.sh
 chown -R $NEW_USER:users /home/$NEW_USER/.config /home/$NEW_USER/.profile
 popd >/dev/null
 rm -rf SwayEOS
