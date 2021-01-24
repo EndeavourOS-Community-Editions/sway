02d4a89da6268652ebcb7bb449ae52b646184098
morten-b <32033374+morten-b@users.noreply.github.com>
2021-01-24 14:26:52 +0100

    Added lightdm greeter conf

--- testinstall.sh
+++ testinstall.sh
@@ -8,5 +8,6 @@ cp .gtkrc-2.0 ~/.gtkrc-2.0
 chmod -R +x ~/.config/sway/scripts
 chmod -R +x ~/.config/waybar/scripts
 sudo pacman -Syu --needed --noconfirm - < packages-repository.txt
+sudo cp lightdm-gtk-greeter.conf /etc/lightdm/
 systemctl enable lightdm
 systemctl start lightdm
