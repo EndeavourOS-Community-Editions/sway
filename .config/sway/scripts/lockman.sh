1b7076b9c51b8c1a8414daaa8d8ad2ab818db48e
morten-b <32033374+morten-b@users.noreply.github.com>
2021-01-25 10:43:38 +0100

    Use config file for configuration of swaylock

--- .config/sway/scripts/lockman.sh
+++ .config/sway/scripts/lockman.sh
@@ -4,6 +4,6 @@ swayidle \
     timeout  5 'swaymsg "output * dpms off"' \
     resume 'swaymsg "output * dpms on"' &
 # Locks the screen immediately
-swaylock -i ~/.config/sway/swaylock.png -L -K --font UbuntuMono -s tile
+swaylock
 # Kills last background task so idle timer doesn't keep running
 kill %%
