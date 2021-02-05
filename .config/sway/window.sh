9b026da9f26daa724b41fb663579d755d9a0a522
OdiousImp2604 <74424076+OdiousImp2604@users.noreply.github.com>
2021-02-05 15:40:28 +0000

    Delete .config/sway directory

--- .config/sway/window.sh
+++
@@ -1,7 +0,0 @@
-swaymsg -t get_tree |
-         jq -r '.nodes[].nodes[] | if .nodes then [recurse(.nodes[])] else [] end + .floating_nodes | .[] |
-       select(.nodes==[]) | ((.id | tostring) + "" + .name)' |
-         wofi --show dmenu | {
-           read -r id name
-           swaymsg "[con_id=$id]" focus
-       }
