12a520ef8652c49ca2bb30261ea2ee13aa333b14
OdiousImp2604 <74424076+OdiousImp2604@users.noreply.github.com>
2021-02-12 14:40:54 +0000

    Add files via upload

---
+++ .config/waybar/scripts/keyhint.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+yad --title="EndeavourOS Sway-WM keybindings:" --no-buttons --geometry=400x345-15-400 --list --column=key: --column=description: --column=command: "ESC" "close this app" "" "=" "modkey" "(set mod Mod4)" "+enter" " floating terminal" "(xfce4-terminal)" "+o" "open Browser" "" "+n" "open Filebrowser" "" "+Shift+Return" "fixed terminal" "(termite)" "+q" "close focused app" "(kill)" "[Shift]+Print-key" "screenshot" "(grim)" "+Shift+e" "power-menu" "(wofi)" "+F1" "open keybinding helper" "full list"
