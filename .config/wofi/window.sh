73f771728812485edf772e7bdc5833ed56fa2ca7
OdiousImp2604 <74424076+OdiousImp2604@users.noreply.github.com>
2021-03-28 14:15:46 +0100

    Delete window.sh

--- .config/wofi/window.sh
+++
@@ -1,6 +0,0 @@
-swaymsg -t get_tree |
-	jq -r '.nodes[].nodes[] | if .nodes then [recurse(.nodes[])] else [] end + .floating_nodes | .[] | select(.nodes==[]) | ((.id | tostring) + "" + .name)' |
-	wofi -c ~/.config/wofi/menu -s ~/.config/wofi/style.css --show dmenu --lines 4 | {
-	read -r id name
-	swaymsg "[con_id=$id]" focused
-}
