d0125eec5df35b7d7fe51944b41fd438f26bec72
OdiousImp2604 <74424076+OdiousImp2604@users.noreply.github.com>
2021-05-02 14:43:14 +0100

    Update lockman.sh

--- .config/sway/scripts/lockman.sh
+++ .config/sway/scripts/lockman.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Times the screen off and puts it to background
 swayidle \
-    timeout  5 'swaymsg "output * dpms off"' \
+    timeout  300 'swaymsg "output * dpms off"' \
     resume 'swaymsg "output * dpms on"' &
 # Locks the screen immediately
 swaylock
