cc5e9f14c4d2ca239e463bcbecbb7344a59d2e23
OdiousImp2604 <74424076+OdiousImp2604@users.noreply.github.com>
2021-08-08 08:59:39 +0000

    Update set_once.sh

--- set_once.sh
+++ set_once.sh
@@ -3,6 +3,6 @@ dbus-launch dconf load / < ~/xed.dconf
 chmod -R +x ~/.config/sway/scripts
 chmod -R +x ~/.config/waybar/scripts
 chmod -R +x ~/.config/wofi/windows.py
-sed -i 's|\(exec --no-startup-id ~/set_once.sh\)|# \1|' ~/.config/sway/config
+sed -i 's|\(exec ~/set_once.sh\)|# \1|' ~/.config/sway/config
 rm ~/xed.dconf
 rm ~/set_once.sh
