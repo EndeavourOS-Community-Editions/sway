260cd3a15866b4aa61503e31b7519becb39a1154
Ian Wright <idwright90@gmail.com>
2022-08-21 15:00:20 -0400

    Remove angle brackets from laptop display name

--- .config/sway/scripts/clamshell.sh
+++ .config/sway/scripts/clamshell.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 if cat /proc/acpi/button/lid/*/state | grep -q open; then
-    swaymsg output <eDP-1> enable
+    swaymsg output eDP-1 enable
 else
-    swaymsg output <eDP-1> disable
+    swaymsg output eDP-1 disable
 fi
