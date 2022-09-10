9b0a9e684fb1546162ed967c646656237bed936d
wouter9000 <110843046+wouter9000@users.noreply.github.com>
2022-09-10 14:07:54 +0200

    Screenshots are now both saved and copied
    Screenshot is now both saved and copied instead of only being saved.

--- .config/sway/scripts/screenshot.sh
+++ .config/sway/scripts/screenshot.sh
@@ -6,13 +6,18 @@ selected=$(printf '%s\n' $entries | wofi --style=$HOME/.config/wofi/style.widget
  
 case $selected in
   active)
-    /usr/share/sway/scripts/grimshot --notify save active;;
+    /usr/share/sway/scripts/grimshot --notify save active  
+    /usr/share/sway/scripts/grimshot copy active;;         
   screen)
-    /usr/share/sway/scripts/grimshot --notify save screen;;
+    /usr/share/sway/scripts/grimshot --notify save screen  
+    /usr/share/sway/scripts/grimshot copy screen;;
   output)
-    /usr/share/sway/scripts/grimshot --notify save output;;
+    /usr/share/sway/scripts/grimshot --notify save output  
+    /usr/share/sway/scripts/grimshot copy output;;
   area)
-    /usr/share/sway/scripts/grimshot --notify save area;;
+    /usr/share/sway/scripts/grimshot --notify save area  
+    /usr/share/sway/scripts/grimshot copy area;;  
   window)
-    /usr/share/sway/scripts/grimshot --notify save window;;
+    /usr/share/sway/scripts/grimshot --notify save window  
+    /usr/share/sway/scripts/grimshot copy window;;
 esac
