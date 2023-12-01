4ef9ed6150c68037b0666abe245bd65eaab52f74
fhoekstra <32362869+fhoekstra@users.noreply.github.com>
2023-12-01 14:09:33 +0100

    fix: source .profile on session launch

--- sway-install.sh
+++ sway-install.sh
@@ -7,5 +7,9 @@ cp .profile ~/.profile
 cp .gtkrc-2.0 ~/.gtkrc-2.0
 chmod -R +x ~/.config/sway/scripts
 chmod -R +x ~/.config/waybar/scripts
+
+# Insert `source ~/.profile` after the line that defines SDDM_USER_SHELL in wayland-session
+sudo sed -ie '/^SDDM_USER_SHELL/a source ~\/.profile' /usr/share/sddm/scripts/wayland-session
+
 sudo pacman -Syu --needed --noconfirm - < packages-repository.txt
 dbus-launch dconf load / < xed.dconf
