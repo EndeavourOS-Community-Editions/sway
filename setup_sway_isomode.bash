611caaf955aadd517f2fd5091360f94238821104
Johannes Kamprad <archlinux@kamprad.net>
2024-04-10 13:38:51 +0200

    Create setup_sway_isomode.bash

---
+++ setup_sway_isomode.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+username="$1"
+git clone https://github.com/EndeavourOS-Community-Editions/sway.git
+
+cd sway
+cp -R .config /home/$username/                                               
+chmod -R +x /home/$username/.config/sway/scripts
+chmod -R +x /home/$username/.config/waybar/scripts
+chmod +x /home/$username/.config/wofi/windows.py
+cp .profile /home/$username/
+chown -R $username:$username /home/$username/.config
+chown $username:$username /home/$username/.profile
+cd ..
+rm -rf sway
+wget https://raw.githubusercontent.com/EndeavourOS-Community-Editions/sway/main/packages-repository.txt
+pacman -S --needed --noconfirm - < packages-repository.txt
+systemctl enable sddm
